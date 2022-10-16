ec72da970da7ac7b23cce51b54e52c9b827068bf
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-17 00:32:35 +0800

    small fix

--- encrypt1.py
+++ encrypt1.py
@@ -5,8 +5,8 @@ from eth_account.messages import encode_defunct
 
 def encryptWithPublicKey(type, privateKey):
 
-    with open('encrypted.bin', 'r') as f:
+    with open('encrypted.bin', 'rb') as f:
         msg = f.read()
-        message = encode_defunct(text=msg)
+        message = encode_defunct(text=str(msg))
     signed_message = w3.eth.account.sign_message(message, private_key=privateKey)
     with open('signed.bin')
