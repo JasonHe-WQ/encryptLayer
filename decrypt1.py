3297672ff300123e20c6d851c0b11540dc390497
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-17 23:34:06 +0800

    small fix

--- decrypt1.py
+++ decrypt1.py
@@ -3,12 +3,13 @@ import ecies
 
 
 def decryptWithPrivateKey(privateKey):
-
     with open('encryptedData.bin', "rb") as f:
         encryptedData = f.read()
     privateKey = bytes(privateKey)
-    data = ecies.decrypt(privateKey,encryptedData)
-    with open('text.txt','w') as f:
+    data = ecies.decrypt(privateKey, encryptedData)
+    with open('text.bin', 'wb') as f:
         f.write(data)
     print(data)
+
+
 print(decryptWithPrivateKey(0x0f0a9f071c868b2a75a504c29c2f93ceb7464a8f0d9f768abaced69a66d90713))
