72831a943b80b8532fac517a4cf9ebea7eed3f13
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-17 17:40:03 +0800

    small fix

--- ESADecrypt.py
+++ ESADecrypt.py
@@ -1,5 +1,7 @@
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
 def ESADecrypt(password):
-    pass
+    with open("encryptedData.bin", "rb") as f:
+        nonce, tag, ciphertext = [f.read(x) for x in (16, 16, -1)]
+    cipher = AES.new(password, AES.MODE_EAX, nonce)
+    data = cipher.decrypt_and_verify(ciphertext, tag)
+    return data, True
--- ESAEncrypt.py
+++ ESAEncrypt.py
@@ -3,14 +3,14 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad
 
 def encryptWithPassword():
-    key = get_random_bytes(16)
+    password = get_random_bytes(16)
     with open('ESAPassword.bin','wb') as f:
-        f.write(key)
-    cipher = AES.new(key, AES.MODE_EAX)
+        f.write(password)
+    cipher = AES.new(password, AES.MODE_EAX)
     with open('data.txt','r') as f:
         msg = f.read().encode('utf-8')
     bytesOfMsg = bytes(msg)
     ciphertext, tag = cipher.encrypt_and_digest(pad(data_to_pad=bytesOfMsg,block_size=AES.block_size))
     with open('encryptedData.bin','wb') as f:
         [ f.write(x) for x in (cipher.nonce, tag, ciphertext) ]
-    return key, cipher.nonce, tag, ciphertext
+    return password, cipher.nonce, tag, ciphertext
