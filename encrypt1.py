3619d1c66bd3e26c993bbb647046404a809230e7
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-17 10:38:28 +0800

    small fix

--- encrypt1.py
+++ encrypt1.py
@@ -1,20 +1,14 @@
 # Signing with the private key
 from web3.auto import w3
 from eth_account.messages import encode_defunct
-
+import pickle
 
 def signWithPrivateKey(privateKey):
     with open('encrypted.txt', 'r') as f:
         msg = f.read()
         message = encode_defunct(text=msg)
     signed_message = w3.eth.account.sign_message(message, private_key=privateKey)
-    msgHash, r, s, v, sig = signed_message
-    with open('msgHash.bin', 'wb') as f:
-        f.write(msgHash)
-    with open('signature.bin', 'wb') as f:
-        f.write(sig)
-    with open('rsv.txt', 'w') as f:
-        f.write(str(r) + '\n')
-        f.write(str(s) + '\n')
-        f.write(str(v) + '\n')
-
+    f = open('signed_message.bin', 'wb')
+    pickle.dump(signed_message, f)
+    f.close()
+    print('Signed')
