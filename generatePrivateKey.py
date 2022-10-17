75ed17995e0fe72e7a5180d0d469930db61535a4
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 01:01:30 +0800

    small fix

--- generatePrivateKey.py
+++ generatePrivateKey.py
@@ -7,5 +7,4 @@ def generate():
     privateKey = acct.key
     privateKeyInHex = privateKey.hex()
     print(privateKeyInHex +'   Save It And Not Share It')
-    print("Address:", acct.address)
     return privateKey, privateKeyInHex, acct.address, acct
