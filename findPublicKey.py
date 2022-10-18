7368855b401478b09b7549e855a3c16b3fbb7b83
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 19:22:37 +0800

    small fix

--- findPublicKey.py
+++ findPublicKey.py
@@ -1,3 +1,9 @@
 import requests
-def find(addr):
-    url = 'https://api.covalenthq.com/v1/'
+
+
+def find(chainID, addr):
+    addr = hex(addr)
+    url = 'https://www.covalenthq.com/docs/api/#get-/v1/{0}/address/{1}/transfers_v2/'.format(chainID, str(addr))
+    return url
+
+print(find(1,0x5568BC7EebC605A88e247769c4acA92d95BC9360))
--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class mailbox():
         'encryptedData.txt' with the receiver's address
         :param senderPublicKey:When the parameter 'Type' is set as RSA and transactions hash can't be found on chain,
          please provide the senderPublicKey of receiver
-        :param senderAddr:When the parameter 'Type' is set as RSA, please provide the address of receiver
+        :param senderAddr:When the parameter 'Type' is set as RSA, please provide the address of receiver in Hex
         :param Type:'RSA' for default Ethereum encrypt function, 'ESA' for using same password both sides knew
         :return:
         """
@@ -69,13 +69,13 @@ class mailbox():
                 if senderPublicKey is None:
                     self.senderPublicKey = self.publicKey
             else:
-                flag, chainName = checkIfTransacted.check(self.senderAddr)
+                flag, chainID = checkIfTransacted.check(self.senderAddr)
                 """
                 If the sender has made any tx, you can get the public key. Else, you can only send message to
                 who revealed the public key.
                 """
                 if flag is True:
-                    self.senderPublicKey = findPublicKey.find(self.senderAddr)
+                    self.senderPublicKey = findPublicKey.find(chainID,self.senderAddr)
             self.encryptedBytes = encrypt2.encryptWithPublicKey(self.senderPublicKey)
 
         elif self.encryptType == 'ESA':
