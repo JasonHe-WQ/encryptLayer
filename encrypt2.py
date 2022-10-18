1644a1b417a0a68c2c558e47d0fee056eab635cf
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 08:48:31 +0800

    small fix

--- encrypt2.py
+++ encrypt2.py
@@ -1,13 +1,12 @@
 # Encrypting with your public key
 import ecies
 
+
 def encryptWithPublicKey(publicKey):
-    with open('data.txt','r') as f:
+    with open('data.txt', 'r') as f:
         msg = f.read().encode('utf-8')
-    encryptedData = ecies.encrypt(str(publicKey),bytes(msg))
-    with open('encryptedData.bin','wb') as f:
+    encryptedData = ecies.encrypt(str(publicKey), bytes(msg))
+    with open('encryptedData.bin', 'wb') as f:
         f.write(encryptedData)
     print('The encrypted data has been saved as "encryptedData.bin"')
     return encryptedData
-
-
