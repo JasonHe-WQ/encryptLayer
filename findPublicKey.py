93491a67a0446e1748e502572b7585ff017f6b43
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 20:23:00 +0800

    small fix

--- findPublicKey.py
+++ findPublicKey.py
@@ -2,25 +2,35 @@ import requests
 
 explorer = {
     '1': 'https://api.etherscan.io',
-    '56':'https://bscscan.com',
+    '56':'https://api.bscscan.com/',
     '43114':'https://snowtrace.io',
     '137':'https://polygonscan.com',
     '42161':'https://arbiscan.io'
 
 }
 
+myToken = {
+    '1': 'VYDCJ3TMWJKTS1FRXVHEWQRSZE6D8HRWI9',
+    '56':'5QXV3MKW67M4S53WHFDFQ9DUUSSN8RW1GI',
+    '43114':'https://snowtrace.io',
+    '137':'https://polygonscan.com',
+    '42161':'https://arbiscan.io'
 
+}
 def find(chainID, addr):
     addr = hex(addr)
-    myToken = 'VYDCJ3TMWJKTS1FRXVHEWQRSZE6D8HRWI9'
-    url = '{}/api?module=account&action=txlist&address={}&startblock=0&' \
-          'endblock=99999999&page=1&offset=1&sort=asc&apikey={}'.format(explorer[chainID],str(addr), myToken)
+    url = '{}/api?module=account' \
+          '&action=txlist' \
+          '&address={}&startblock=0&' \
+          'endblock=99999999' \
+          '&page=1' \
+          '&offset=1' \
+          '&sort=asc&apikey={}'.format(explorer[chainID],str(addr), myToken[chainID])
+    print(requests.get(url))
     data = requests.get(url).json()
     tx = data['result'][0]['hash']
     print(tx)
-    # with open('get.json', 'w') as f:
-    #     f.write(data)
     return url
 
 
-print(find('1', 0x5568BC7EebC605A88e247769c4acA92d95BC9360))
+print(find('56', 0x5568BC7EebC605A88e247769c4acA92d95BC9360))
