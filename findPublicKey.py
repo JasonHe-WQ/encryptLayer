0e2063fe9b2252db26162d065fb7c355f976f09d
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 20:02:05 +0800

    small fix

--- findPublicKey.py
+++ findPublicKey.py
@@ -1,9 +1,17 @@
 import requests
+import json
 
 
 def find(chainID, addr):
     addr = hex(addr)
-    url = 'https://www.covalenthq.com/docs/api/#get-/v1/{0}/address/{1}/transfers_v2/'.format(chainID, str(addr))
+    myToken = 'VYDCJ3TMWJKTS1FRXVHEWQRSZE6D8HRWI9'
+    url = 'https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock=0&' \
+          'endblock=99999999&page=1&offset=1&sort=asc&apikey={}'.format(str(addr),myToken)
+    data = requests.get(url).json()
+    print(data)
+    # with open('get.json', 'w') as f:
+    #     f.write(data)
     return url
 
-print(find(1,0x5568BC7EebC605A88e247769c4acA92d95BC9360))
+
+print(find(1, 0x5568BC7EebC605A88e247769c4acA92d95BC9360))
