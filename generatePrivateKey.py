76ce6824547b62d3c865fe323583c8220c3b2466
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-18 09:00:00 +0800

    small fix

--- generatePrivateKey.py
+++ generatePrivateKey.py
@@ -1,10 +1,11 @@
 from eth_account import Account
 
+
 def generate():
-
-
     acct = Account.create()
     privateKey = acct.key
     privateKeyInHex = privateKey.hex()
-    print(privateKeyInHex +'   Save It And Not Share It')
+    print(privateKeyInHex + '   Save It And Not Share It')
+    with open('privateKeyInHex.txt','w') as f:
+        f.write(privateKeyInHex)
     return privateKey, privateKeyInHex, acct.address, acct
