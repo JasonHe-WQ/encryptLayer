d906c40e6bbec58c1fe475ad8a297e837104aef2
SatoshiVision <wenqinghe1016@gmail.com>
2022-10-20 09:08:55 +0800

    small fix

--- sentToBlockChain.py
+++ sentToBlockChain.py
@@ -3,10 +3,12 @@ import requests
 explorer = {
     '1': 'https://api.etherscan.io',
     '137': 'https://api.polygonscan.com',
+    '8001': 'https://api-testnet.polygonscan.com/'
 }
 myToken = {
     '1': 'VYDCJ3TMWJKTS1FRXVHEWQRSZE6D8HRWI9',
     '137': '2KTHMFT4CIZ9WDIQWQIK9ESD7I9T1VCG5Q',
+    '8001': '2KTHMFT4CIZ9WDIQWQIK9ESD7I9T1VCG5Q'
 
 }
 dataHeader = {
@@ -14,33 +16,34 @@ dataHeader = {
 
 
 def send(chainID, yourAddress, toAddress, privateKeyInBytes):
-    chainIDList = ['1', '137', '56']
-    for i in chainIDList:
-        APIList = {
-            '1': 'https://rpc.ankr.com/eth',
-            '137': 'https://polygon-rpc.com',
-        }
-        with open('encryptedData.bin', 'rb') as f:
-            encryptedDataBytes = f.read()
-        w3 = Web3(Web3.HTTPProvider(APIList[i]))
-        gasOracleUrl = 'https://{}/api?module=gastracker&action=gasoracle&apikey={}'.format(explorer[chainID],
-                                                                                      myToken[chainID])
-        data = requests.get(gasOracleUrl, headers=dataHeader).json()
-        gasPrice = eval(data['result']['ProposeGasPrice'])*1000000000
-        rawHex = w3.eth.account.sign_transaction(dict(
-            nonce=w3.eth.get_transaction_count(yourAddress),
-            maxFeePerGas=gasPrice,
-            maxPriorityFeePerGas=2000000000,
-            gas=200000,
-            to=toAddress,
-            value=0,
-            data=encryptedDataBytes,
-            type=2,  # (optional) the type is now implicitly set based on appropriate transaction params
-            chainId=eval(chainID),
-        ),
-            privateKeyInBytes,
-        )
-        url = 'https://{}?' \
-              'module=proxy&action=eth_sendRawTransaction' \
-              '&hex={}' \
-              '&apikey={}'.format(explorer[chainID], rawHex, myToken[chainID])
+    APIList = {
+        '1': 'https://rpc.ankr.com/eth',
+        '137': 'https://polygon-rpc.com',
+        '8001': 'https://rpc-mumbai.maticvigil.com'
+    }
+    with open('encryptedData.bin', 'rb') as f:
+        encryptedDataBytes = f.read()
+    w3 = Web3(Web3.HTTPProvider(APIList[chainID]))
+    gasOracleUrl = '{}/api?module=gastracker&action=gasoracle&apikey={}'.format(explorer[chainID],
+                                                                                  myToken[chainID])
+    data = requests.get(gasOracleUrl, headers=dataHeader).json()
+    gasPrice = int(eval(data['result']['ProposeGasPrice'])*1000000000)
+    rawHex = w3.eth.account.sign_transaction(dict(
+        nonce=w3.eth.get_transaction_count(yourAddress),
+        maxFeePerGas=gasPrice,
+        maxPriorityFeePerGas=2000000000,
+        gas=200000,
+        to=toAddress,
+        value=0,
+        data=encryptedDataBytes,
+        type=2,  # (optional) the type is now implicitly set based on appropriate transaction params
+        chainId=eval(chainID),
+    ),
+        privateKeyInBytes,
+    )
+    print(rawHex['rawTransaction'])
+    url = 'https://{}?' \
+          'module=proxy&action=eth_sendRawTransaction' \
+          '&hex={}' \
+          '&apikey={}'.format(explorer[chainID], rawHex, myToken[chainID])
+send('137','0x5568BC7EebC605A88e247769c4acA92d95BC9360','0x5568BC7EebC605A88e247769c4acA92d95BC9360','0xf246f79dfc201c11889f4d0ba7fa44e210f80cf8d35ca38d7dc2663fa9900a79')
